5ea750227bf812ab569a04ef518e2e1230ccc28e
ehanstad <ehanstad@kth.se>
2020-10-29 14:42:02 +0100

    implemented first draft of runnable to the sever

--- LAB1/ChatServer.java
+++ LAB1/ChatServer.java
@@ -1,19 +1,40 @@
 import java.io.*;
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.net.*;
+import java.lang.*;
 
-public class ChatServer{
-    public static void main(String[] args) throws Exception{
+public class ChatServer implements Runnable {
+
+  private Socket cs;
+
+  public ChatServer(Socket cs) {
+    this.cs = cs;
+  }
+
+  public void run() {
+    try{
+      Socket s = null;
+      String text = "";
+      while( (s = cs) != null){
+          BufferedReader indata =
+                  new BufferedReader(new InputStreamReader(s.getInputStream()));
+          while( (text = indata.readLine()) != null){
+              System.out.println("Received: " + text);
+              System.out.println("From: " + s.getLocalAddress());
+          }
+      s.shutdownInput();
+      }
+    } catch (IOException e) {
+        System.out.println("ajaj");
+    }
+  }
+
+    public static void main(String[] args) throws Exception {
         ServerSocket ss = new ServerSocket(1234);
-        Socket s = null;
-        String text = "";
-        while( (s = ss.accept()) != null){
-            BufferedReader indata =
-                    new BufferedReader(new InputStreamReader(s.getInputStream()));
-            while( (text = indata.readLine()) != null){
-                System.out.println("Received:" + text);
-            }
-        s.shutdownInput();
+        System.out.println("Server is open on port 1234");
+        while(true) {
+          Socket cs = ss.accept();
+          Runnable r = new ChatServer(cs);
+          (new Thread(r)).start();
         }
     }
 }
