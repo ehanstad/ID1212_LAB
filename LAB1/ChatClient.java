cfeb22d885b2f256a273b8a3d915027fc75360d3
ehanstad <ehanstad@kth.se>
2020-10-29 13:55:55 +0100

    add init code from lecture

---
+++ LAB1/ChatClient.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ChatClient{
+    public static void main(String[] args) throws Exception{
+        Socket s = new Socket("localhost",1234);
+        PrintStream out = new PrintStream(s.getOutputStream());
+        BufferedReader indata = new BufferedReader(new InputStreamReader(System.in));
+        String text;
+        System.out.print("Enter text to send: ");
+        while( (text = indata.readLine()) != null){
+            out.println(text);
+            System.out.print("Enter text to send: ");
+        }
+        s.shutdownOutput();
+    }
+}
---
+++ LAB1/ChatServer.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class ChatServer{
+    public static void main(String[] args) throws Exception{
+        ServerSocket ss = new ServerSocket(1234);
+        Socket s = null;
+        String text = "";
+        while( (s = ss.accept()) != null){
+            BufferedReader indata =
+                    new BufferedReader(new InputStreamReader(s.getInputStream()));
+            while( (text = indata.readLine()) != null){
+                System.out.println("Received:" + text);
+            }
+        s.shutdownInput();
+        }
+    }
+}
