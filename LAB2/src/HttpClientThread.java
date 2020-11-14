b8550d586459e9cba1eac926af8f7b35762aca6e
Lucas Villarroel <vilucas7@gmail.com>
2020-11-14 16:48:03 +0100

    Refactor html/http code, style changes

--- LAB2/src/HttpClientThread.java
+++ LAB2/src/HttpClientThread.java
@@ -8,74 +8,34 @@ public class HttpClientThread extends Thread {
   private ServerSocket server;
   private Guess instance;
 
-  public HttpClientThread(Socket socket, ServerSocket server){
+  public HttpClientThread(Socket socket, ServerSocket server) {
     this.socket = socket;
     this.server = server;
     this.instance = new Guess();
   }
 
   private void createPostResault(PrintStream res, StringTokenizer reqTokens) throws IOException {
-    while(reqTokens.hasMoreTokens()){
-      if((reqTokens.nextToken()).contains("guessedNumber")) {
-        int guessedNumber = Integer.parseInt(reqTokens.nextToken());
-        this.instance.setGuessedNumber(guessedNumber);
+    while (reqTokens.hasMoreTokens()) {
+      if ((reqTokens.nextToken()).contains("guessedNumber")) {
+        this.instance.setGuessedNumber(Integer.parseInt(reqTokens.nextToken()));
       }
     }
-    short indicator = this.instance.checkGuess(this.instance.getGuessedNumber());
-    res.println("HTTP/1.1 200 OK");
-    res.println("Server: Counting game 1.0");
-    res.println("Content-Type: text/html");
-    res.println();
-
-    res.println("<html><body>");
-    if(indicator!=0) {
-      if(indicator==-1){
-        res.println("<h2>Nope, guess higher. You have made ");
-      } else {
-        res.println("<h2>Nope, guess lower. You have made ");
-      }
-      res.println(this.instance.getNoGuesses() + " guess(es)</h2>");
-      res.println("What's your guess?");
-      res.println("<form action='/send' method='post' >");
-      res.println("<input type='text' name='guessedNumber' />");
-      res.println("<input type='submit' value='submit' /></form>");
-    } else {
-      res.println("<h2>Correct</h2>");
-    }
-    res.println("</html></body>");
+    int noGuesses = this.instance.getNoGuesses();
+    int guessedNumber = this.instance.getGuessedNumber();
+    short cmp = this.instance.checkGuess(guessedNumber);
+    res.println(HttpGenerator.getGuessRes(guessedNumber, noGuesses, cmp));
   }
 
   private void createGetResault(PrintStream res, StringTokenizer reqTokens) throws IOException {
     String path = reqTokens.nextToken();
-    File file = new File(".." + path);
-
     if (!"/favicon".equals(path)) {
-      if (file.exists() && !file.isDirectory()) {
-        res.println("HTTP/1.1 200 OK");
-        res.println("Server: Counting game 1.0");
-        if (path.indexOf(".html") != -1)
-          res.println("Content-Type: text/html");
-        if (path.indexOf(".gif") != -1)
-          res.println("Content-Type: image/gif");
-        res.println();
-
-        FileInputStream in = new FileInputStream(file);
-        byte[] b = new byte[1024];
-        while (in.available() > 0) {
-          res.write(b, 0, in.read(b));
-        }
+      if ("/index.html".equals(path)) {
+        System.out.println(HttpGenerator.getStartRes());
+        res.println(HttpGenerator.getStartRes());
       } else {
-        createBadResault(res);
+        res.println(HttpGenerator.getNotFoundRes());
       }
     }
-  }
-
-  private void createBadResault(PrintStream res) {
-    res.println("HTTP/1.1 404 Not Found");
-    res.println("Server: Counting game 1.0");
-    res.println("Content-Type: text/html");
-    res.println();
-    res.println("404 Not Found");
   }
 
   void handleRequest(String req) throws IOException {
@@ -83,29 +43,29 @@ public class HttpClientThread extends Thread {
     StringTokenizer reqTokens = new StringTokenizer(req, " =?");
     String method = reqTokens.nextToken();
 
-    if("POST".equals(method)) {
+    if ("POST".equals(method)) {
       createPostResault(res, reqTokens);
-    } else if("GET".equals(method)) {
+    } else if ("GET".equals(method)) {
       createGetResault(res, reqTokens);
     } else {
-      createBadResault(res);
+      res.println(HttpGenerator.getNotFoundRes());
     }
     this.socket.shutdownOutput();
     this.socket.close();
   }
 
   public void run() {
-    while(!this.socket.isClosed()) {
+    while (!this.socket.isClosed()) {
       try {
         BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         StringBuilder req = new StringBuilder();
 
         do {
           req.append((char) this.socket.getInputStream().read());
-        } while(this.socket.getInputStream().available()>0);
+        } while (this.socket.getInputStream().available() > 0);
         handleRequest(req.toString());
 
-      } catch(Exception e) {
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
